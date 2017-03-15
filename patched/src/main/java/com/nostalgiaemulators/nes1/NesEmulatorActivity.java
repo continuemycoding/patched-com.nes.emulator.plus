2c8f237b22c13f51bee96be38c3248876a4e25d7
llq <664301573@qq.com>
2017-03-16 00:01:17 +0800

    游戏菜单修改

---
+++ patched/src/main/java/com/nostalgiaemulators/nes1/NesEmulatorActivity.java
@@ -0,0 +1,36 @@
+package com.nostalgiaemulators.nes1;
+
+import com.nostalgiaemulators.framework.Emulator;
+import com.nostalgiaemulators.framework.base.EmulatorActivity;
+import com.nostalgiaemulators.framework.base.GameMenu;
+
+import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexIgnore;
+import lanchon.dexpatcher.annotation.DexReplace;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+@DexEdit
+public class NesEmulatorActivity extends EmulatorActivity {
+
+    @DexIgnore
+    public NesEmulatorActivity(){}
+
+    @DexReplace
+    @Override
+    public void onGameMenuPrepare(GameMenu gameMenu)
+    {
+        gameMenu.add(R.string.game_menu_reset, R.drawable.ic_reset);
+        gameMenu.add(R.string.game_menu_save, R.drawable.ic_save);
+        gameMenu.add(R.string.game_menu_load, R.drawable.ic_load);
+        gameMenu.add(R.string.game_menu_settings, R.drawable.ic_game_settings);
+    }
+
+    @DexIgnore
+    @Override
+    public Emulator getEmulatorInstance() {
+        return null;
+    }
+}
