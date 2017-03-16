5a549547b33df8312bf73e63372c924e7f9f8a8a
llq <664301573@qq.com>
2017-03-16 22:02:00 +0800

    NES 模拟器

--- patched/src/main/java/com/nostalgiaemulators/nes1/NesGalleryActivity.java
+++ patched/src/main/java/com/nostalgiaemulators/nes1/NesGalleryActivity.java
@@ -1,25 +1,13 @@
 package com.nostalgiaemulators.nes1;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-
 import com.nostalgiaemulators.framework.Emulator;
 import com.nostalgiaemulators.framework.base.EmulatorActivity;
 import com.nostalgiaemulators.framework.ui.gamegallery.GalleryActivity;
-import com.nostalgiaemulators.framework.ui.gamegallery.GameDescription;
-import com.qiang.framework.helper.FileHelper;
-import com.qiang.framework.helper.SystemHelper;
 
 import java.util.Set;
 
-import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexIgnore;
-import lanchon.dexpatcher.annotation.DexReplace;
-
-import com.qiang.nes.R;
 
 /**
  * Created by Administrator on 2017/3/16.
@@ -30,26 +18,6 @@ public class NesGalleryActivity extends GalleryActivity{
 
     @DexIgnore
     public NesGalleryActivity(){}
-
-    @DexAdd
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        String path = Environment.getDataDirectory() + "/data/" + getPackageName() + "/game.zip";
-        FileHelper.copyResource(this, R.raw.game, path);
-
-        GameDescription gameDescription = new GameDescription();
-        gameDescription.name = "";
-        gameDescription.path = path;
-
-        Intent intent = new Intent(this, NesEmulatorActivity.class);
-        intent.putExtra("game", gameDescription);
-        intent.putExtra("slot", 0);
-        intent.putExtra("fromGallery", false);
-        startActivity(intent);
-    }
 
     @DexIgnore
     @Override
