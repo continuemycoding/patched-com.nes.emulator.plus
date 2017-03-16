e278b15b46b309f39aee26d9502d4a4585f98ddb
llq <664301573@qq.com>
2017-03-16 23:22:05 +0800

    定制菜单

--- patched/src/main/java/com/nostalgiaemulators/nes1/NesGalleryActivity.java
+++ patched/src/main/java/com/nostalgiaemulators/nes1/NesGalleryActivity.java
@@ -2,10 +2,13 @@ package com.nostalgiaemulators.nes1;
 
 import com.nostalgiaemulators.framework.Emulator;
 import com.nostalgiaemulators.framework.base.EmulatorActivity;
+import com.nostalgiaemulators.framework.base.GameMenu;
 import com.nostalgiaemulators.framework.ui.gamegallery.GalleryActivity;
+import com.nostalgiaemulators.framework.utils.Utils;
 
 import java.util.Set;
 
+import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexIgnore;
 
@@ -18,6 +21,23 @@ public class NesGalleryActivity extends GalleryActivity{
 
     @DexIgnore
     public NesGalleryActivity(){}
+
+    @DexAdd
+    @Override
+    public void onGameMenuPrepare(GameMenu gameMenu)
+    {
+        gameMenu.add(R.string.gallery_menu_download, R.drawable.ic_file_download);
+        //gameMenu.add(R.string.gallery_menu_language, R.drawable.ic_language);
+        //if (Utils.getDeviceType(this) == Utils.ServerType.mobile) {
+        //    gameMenu.add(R.string.gallery_menu_remote_control, R.drawable.ic_gamepad);
+        //}
+        gameMenu.add(R.string.gallery_menu_reload, R.drawable.ic_reset);
+        //gameMenu.add(R.string.gallery_menu_load_from_dir, R.drawable.ic_folder);
+        gameMenu.add(R.string.gallery_menu_pref, R.drawable.ic_game_settings);
+        //gameMenu.add(R.string.gallery_menu_recommend, R.drawable.ic_recommend);
+        //gameMenu.add(R.string.gallery_menu_rate_us, R.drawable.ic_star);
+        //gameMenu.add(R.string.gallery_menu_help, R.drawable.ic_help_mnu);
+    }
 
     @DexIgnore
     @Override
