2ad32fdd593f80918f5543b0066464a533054885
llq <664301573@qq.com>
2017-03-30 01:38:49 +0800

    批量打包

---
+++ 批量打包.bat
@@ -0,0 +1,15 @@
+gradlew :patched:assembleShuangjielong2Dangbei ^
+        :patched:assembleShuangjielong3Dangbei ^
+        :patched:assembleChaojimaliDangbei ^
+        :patched:assembleChaojimali2Dangbei ^
+        :patched:assembleChaojimali3Dangbei ^
+        :patched:assembleLvsebingtuanDangbei ^
+        :patched:assembleShaluomansheDangbei ^
+        :patched:assembleChiseyaosaiDangbei ^
+        :patched:assembleRenzhemaoDangbei ^
+        :patched:assembleRenzhelongjianzhuan2Dangbei ^
+        :patched:assembleRenzhelongjianzhuan3Dangbei ^
+        :patched:assembleZhadanrenDangbei ^
+        :patched:assembleXiaoaojianghuDangbei ^
+        :patched:assembleTubageDangbei ^
+        :patched:assembleKuaidaxuanfengDangbei ^
