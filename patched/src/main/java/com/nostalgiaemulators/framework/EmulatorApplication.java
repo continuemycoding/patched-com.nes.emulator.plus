0200314232c2d824ca0513315e0b328efcf254ad
llq <664301573@qq.com>
2017-03-31 13:12:28 +0800

    当贝广告初始化

--- patched/src/main/java/com/nostalgiaemulators/framework/EmulatorApplication.java
+++ patched/src/main/java/com/nostalgiaemulators/framework/EmulatorApplication.java
@@ -1,10 +1,14 @@
 package com.nostalgiaemulators.framework;
 
+import com.db.android.api.AdSystem;
 import com.nostalgiaemulators.framework.utils.Log;
 import com.qiang.framework.MyApplication;
+import com.qiang.framework.helper.MetaDataHelper;
 import com.qiang.nes.BuildConfig;
 
-import lanchon.dexpatcher.annotation.*;
+import lanchon.dexpatcher.annotation.DexAction;
+import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexReplace;
 
 /**
  * Created by Administrator on 2017/3/30.
@@ -21,6 +25,12 @@ public abstract class EmulatorApplication extends MyApplication {
         initVersionCodes();
 
         super.onCreate();
+
+        AdSystem.getInstance(this).init(MetaDataHelper.getString("DANGBEIAD_APPID"), MetaDataHelper.getString("DANGBEIAD_APPKEY"));
+        //ReflectHelper.invokeMethod("com.db.android.api.AdSystem", "init", new Object[]{MetaDataHelper.getString("DANGBEIAD_APPID"), MetaDataHelper.getString("DANGBEIAD_APPKEY")});
+
+        AdSystem.setLogState(BuildConfig.DEBUG);
+        //ReflectHelper.invokeMethod("com.db.android.api.AdSystem", "setLogState", new Object[]{com.qiang.framework.BuildConfig.DEBUG});
     }
 
     private void initVersionCodes(){}
