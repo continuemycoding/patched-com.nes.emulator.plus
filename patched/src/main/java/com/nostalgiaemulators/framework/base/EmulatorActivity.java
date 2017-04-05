7cc99b79c3d1e6c1261e54a536426d0566d42e0a
llq <664301573@qq.com>
2017-04-05 12:32:20 +0800

    hideTouchController

--- patched/src/main/java/com/nostalgiaemulators/framework/base/EmulatorActivity.java
+++ patched/src/main/java/com/nostalgiaemulators/framework/base/EmulatorActivity.java
@@ -1,7 +1,13 @@
 package com.nostalgiaemulators.framework.base;
 
-import com.nostalgiaemulators.framework.Emulator;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
 
+import com.nostalgiaemulators.framework.Emulator;
+import com.nostalgiaemulators.framework.controllers.TouchController;
+
+import lanchon.dexpatcher.annotation.DexAction;
+import lanchon.dexpatcher.annotation.DexAdd;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexIgnore;
 import lanchon.dexpatcher.annotation.DexReplace;
@@ -10,16 +16,25 @@ import lanchon.dexpatcher.annotation.DexReplace;
  * Created by Administrator on 2017/3/16.
  */
 
-@DexEdit
+@DexEdit(defaultAction = DexAction.IGNORE)
 public abstract class EmulatorActivity extends ImmersiveActivity implements GameMenu.OnGameMenuListener{
 
-    @DexIgnore
     protected Manager manager;
 
-    @DexIgnore
-    public EmulatorActivity(){}
+    private TouchController touchController;
 
-    @DexIgnore
+    @DexEdit(target = "onCreate")
+    protected void source_onCreate(Bundle savedInstanceState){}
+
+    @SuppressLint("MissingSuperCall")
+    @DexAdd
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        source_onCreate(savedInstanceState);
+
+        touchController.hide();
+    }
+
     public abstract Emulator getEmulatorInstance();
 
     @DexReplace
@@ -35,31 +50,26 @@ public abstract class EmulatorActivity extends ImmersiveActivity implements Game
         manager.saveState(10);
     }
 
-    @DexIgnore
     @Override
     public void onGameMenuClosed(GameMenu gameMenu) {
 
     }
 
-    @DexIgnore
     @Override
     public void onGameMenuCreate(GameMenu gameMenu) {
 
     }
 
-    @DexIgnore
     @Override
     public void onGameMenuItemSelected(GameMenu gameMenu, GameMenu.GameMenuItem gameMenuItem) {
 
     }
 
-    @DexIgnore
     @Override
     public void onGameMenuOpened(GameMenu gameMenu) {
 
     }
 
-    @DexIgnore
     @Override
     public void onGameMenuPrepare(GameMenu gameMenu) {
 
